29ff71aa1538e92963c74ac2652c32cbc6e0eafa
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-12 20:45:50 +0000

    created component artist

---
+++ src/front/js/component/artist.js
@@ -0,0 +1,33 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/home.css";
+import { Link } from "react-router-dom";
+
+export const Artist = () => {
+	const { store, actions } = useContext(Context);
+    const [name, setName] = useState('')
+    const [url, setUrl] = useState('')
+
+    function sendData(e){
+        e.preventDefault()
+        actions.createArtist(name, url)
+    }
+
+	return (
+		<div className="container mt-5">
+            <div className="col-md-6">
+                <form>
+                    <div className="mb-3">
+                        <label htmlFor="nameInput" className="form-label">Artist Name</label>
+                        <input value={name} onChange={(e)=>setName(e.target.value)} className="form-control" id="nameInput" aria-describedby="emailHelp" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="urlInput" className="form-label">Image URL</label>
+                        <input value={url} onChange={(e)=>setUrl(e.target.value)} className="form-control" id="urlInput" />
+                    </div>
+                    <button onClick={sendData} type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+		</div>
+	);
+};
