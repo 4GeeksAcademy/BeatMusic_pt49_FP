d41d5253c95df4db7b812df71f0b8e27c29a83a7
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-17 19:48:40 +0000

    created song class

--- src/api/models.py
+++ src/api/models.py
@@ -33,6 +33,7 @@ class Album(db.Model):
             "name": self.name,
             "img_url": self.img_url
         }
+
 class Artist(db.Model):
     __tablename__ = 'artist'
     id = db.Column(db.Integer, primary_key=True)
@@ -48,4 +49,21 @@ class Artist(db.Model):
             "id": self.id,
             "name": self.name,
             "img_url": self.img_url
-        }   
+        }
+    
+class Song(db.Model):
+    __tablename__ = 'song'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    lenght = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<Song %r>' % self.name
+
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "lenght": self.lenght
+        }  
