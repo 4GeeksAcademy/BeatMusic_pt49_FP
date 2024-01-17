ec3a0243e4836bf03bdc4e1cd956351b31331c0b
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-17 17:51:54 +0000

    added updateArtist function

--- src/front/js/component/editArtist.js
+++ src/front/js/component/editArtist.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
 export const EditArtist = () => {
@@ -9,14 +9,26 @@ export const EditArtist = () => {
     const params = useParams();
     const [name, setName] = useState(store.singleArtist.name)
     const [url, setUrl] = useState(store.singleArtist.img_url)
+    const navigate = useNavigate();
     
     useEffect(()=>{
         actions.getSingleArtist(params.artist_id)
     },[])
-    console.log(store.singleArtist.name)
+
+    useEffect(() => {
+        if (store.singleArtist) {
+          setName(store.singleArtist.name);
+          setUrl(store.singleArtist.img_url);
+        }
+      }, [store.singleArtist]);
 
     function sendData(){
-        actions.createArtist(name, url)
+        if (name !== "" && url !== "") {
+            actions.updateArtist(params.artist_id, name, url)
+            navigate("/admin/listartist");
+        } else {
+            alert('Complete all fields. Artist has not been saved.');
+        }
     }
 
 	return (
