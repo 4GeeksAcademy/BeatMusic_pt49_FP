13ea6f17fe9e9cd900d89f9080b93ec7aab646a1
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-18 18:24:40 +0000

    added getSongs to useEffect

--- src/front/js/store/appContext.js
+++ src/front/js/store/appContext.js
@@ -34,6 +34,8 @@ const injectContext = PassedComponent => {
 
 			state.actions.getArtist();
 
+			state.actions.getSong();
+
 		}, []);
 
 		// The initial value for the context is not null anymore, but the current state of this component,
