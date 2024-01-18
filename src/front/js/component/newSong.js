6b2f7fdfdd76221c7ec122a1721f7d86eec95793
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-18 18:26:09 +0000

    created src/front/js/component/newSong.js component

---
+++ src/front/js/component/newSong.js
@@ -0,0 +1,39 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/home.css";
+import { Link, useNavigate } from "react-router-dom";
+
+export const NewSong = () => {
+	const { store, actions } = useContext(Context);
+    const [name, setName] = useState('')
+    const [url, setUrl] = useState('')
+    const navigate = useNavigate();
+
+    function sendData(){
+        if (name !== "" && url !== "") {
+            actions.createArtist(name, url);
+            navigate("/admin/listartist");
+        } else {
+            alert('Complete all fields. Artist has not been saved.');
+        }
+    }
+
+	return (
+		<div className="container mt-5">
+            <h1 className="text-center mt-3">Create new Artist</h1>
+            <div className="col-md-6">
+                <form>
+                    <div className="mb-3">
+                        <label htmlFor="nameInput" className="form-label">Artist Name</label>
+                        <input value={name} onChange={(e)=>setName(e.target.value)} className="form-control" id="nameInput" aria-describedby="emailHelp" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="urlInput" className="form-label">Image URL</label>
+                        <input value={url} onChange={(e)=>setUrl(e.target.value)} className="form-control" id="urlInput" />
+                    </div>
+                    <button onClick={sendData} type="button" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+		</div>
+	);
+};
