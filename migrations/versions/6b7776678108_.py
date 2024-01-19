f541bd34fdbfd79554ab907bd45c9b67041eacf4
DanielMF89 <139999329+DanielMF89@users.noreply.github.com>
2024-01-19 19:01:01 +0000

    buttoms

--- migrations/versions/6b7776678108_.py
+++ migrations/versions/6b7776678108_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: e5701e6a0315
+Revision ID: 6b7776678108
 Revises: 
-Create Date: 2024-01-17 20:14:16.891256
+Create Date: 2024-01-19 18:52:11.611021
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'e5701e6a0315'
+revision = '6b7776678108'
 down_revision = None
 branch_labels = None
 depends_on = None
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
+	const { store, actions } = useContext(Context);
+
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
@@ -9,6 +12,19 @@ export const Navbar = () => {
 					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 				</Link>
 				<div className="ml-auto">
+					{store.auth == true ? null :
+						<Link to="/signup">
+							<button className="btn btn-success">Sign Up</button>
+						</Link>
+					}
+					{store.auth == true ? null :
+						<Link to="/login">
+							<button className="btn btn-primary mx-2">Log In</button>
+						</Link>
+					}
+					{store.auth == false ? null :
+						<button onClick={actions.logout} className="btn btn-danger">Log Out</button>
+					}
 					<Link to="/admin/listalbum">
 						<button className="btn btn-primary mx-1">Admin List Album</button>
 					</Link>
