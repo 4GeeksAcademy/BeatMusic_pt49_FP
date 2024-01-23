587903b8317c4ccb8d31d8b7dc9d07b0e3b1dbb8
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-23 22:24:53 +0000

    added columns to display friends and favorites

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -14,8 +14,40 @@ export const Private = () => {
             {store.auth == false ? <Navigate to="/" /> :
                 <>
                     <h1>Private</h1>
-                    <br></br>
                     <p>Welcome to your private area.</p>
+                    <div className="row">
+                        <div className="col-3 border border-primary rounded">
+                            <h2>Friends</h2>
+                        </div>
+                        <div className="col-3 border border-primary rounded">
+                            <h2>Favorite Artists</h2>
+                            <ul className="list-group">
+                                {store.favoriteArtists.map((item) => {
+                                    return (
+                                        <li key={item.id} className="list-group-item">
+                                            <div className="row">
+                                                <div className="col-2 d-flex align-items-center justify-content-center">
+                                                    <img src={item.img_url} className="img-thumbnail rounded-circle" />
+                                                </div>
+                                                <div className="col-8">
+                                                    <p className="fs-5 fw-bold">{item.name}</p>
+                                                </div>
+                                                <div className="col-2 d-flex align-items-center justify-content-evenly">
+                                                    <button className="btn btn-success">Delete from Favorites</button>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        </div>
+                        <div className="col-3 border border-primary rounded">
+                            <h2>Favorite Albums</h2>
+                        </div>
+                        <div className="col-3 border border-primary rounded">
+                            <h2>Favorite Songs</h2>
+                        </div>
+                    </div>
                 </>
             }
         </div>
