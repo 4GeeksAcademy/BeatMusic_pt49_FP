f1cf5c83105b91c6242f5fa96794e5d00f8a0e57
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-24 20:39:04 +0000

    Added admin

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from .models import db, User, Artist, Album, Song, FavoriteArtist, AdminUser
+from .models import db, User, Artist, Album, Song, AdminUser
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
