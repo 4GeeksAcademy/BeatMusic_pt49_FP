3a5bc44b4ee321d8146272d0686fb22dd26cd9ef
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-24 19:12:28 +0000

    corrected error in component, calling name instead of id

--- src/front/js/component/artists.js
+++ src/front/js/component/artists.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect, } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 import { Link, Navigate, useNavigate } from "react-router-dom";
