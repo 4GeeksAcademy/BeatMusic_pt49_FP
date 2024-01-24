d63a4516da0123396c58ed1a7eabd27d93b6cd6b
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-24 23:29:20 +0000

    finished albums component

--- src/front/js/component/albums.js
+++ src/front/js/component/albums.js
@@ -6,24 +6,24 @@ import { Link, Navigate, useNavigate } from "react-router-dom";
 export const Albums = () => {
 	const { store, actions } = useContext(Context);
 	const navigate = useNavigate();
-	const favorites = store.favoriteArtists.map(item => item.artist)
+	const favorites = store.favoriteAlbums.map(item => item.album)
 
 	useEffect(()=>{
-        actions.getFavoriteArtists(store.userId)
+        actions.getFavoriteAlbums(store.userId)
     },[])
 
 	useEffect(() => {
-        if (store.favoriteArtists) {
-            actions.getFavoriteArtists(store.userId);
+        if (store.favoriteAlbums) {
+            actions.getFavoriteAlbums(store.userId);
         }
-      }, [store.favoriteArtists]);
+      }, [store.favoriteAlbums]);
 
 	return (
 		<div className="container">
-			<h1 className="text-center my-3">Artists</h1>
+			<h1 className="text-center my-3">Albums</h1>
 			{store.auth == false ? <Navigate to="/" /> :
 				<ul className="list-group">
-					{store.artist.map((item) => {
+					{store.album.map((item) => {
 						return (
 							<li key={item.id} className="list-group-item">
 								<div className="row">
@@ -35,10 +35,10 @@ export const Albums = () => {
 									</div>
 									<div className="col-4 d-flex align-items-center justify-content-evenly">
 										{favorites.includes(item.name) ? null :
-											<button onClick={()=> {actions.addFavoriteArtist(item.id)}} className="btn btn-success">Add to Favorites</button>
+											<button onClick={()=> {actions.addFavoriteAlbum(item.id)}} className="btn btn-success">Add to Favorites</button>
 										}
 										{favorites.includes(item.name) ?
-											<button onClick={()=> {actions.deleteFavoriteArtist(item.id)}} className="btn btn-danger">Delete from Favorites</button>
+											<button onClick={()=> {actions.deleteFavoriteAlbum(item.id)}} className="btn btn-danger">Delete from Favorites</button>
 										: null }
 									</div>
 								</div>
