0d7b57961a5eb258ccd4c59021154fb6c180b298
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-24 17:53:44 +0000

    added security, only accesible when logged in

--- src/front/js/component/artists.js
+++ src/front/js/component/artists.js
@@ -1,34 +1,37 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 
 export const Artists = () => {
 	const { store, actions } = useContext(Context);
+	const navigate = useNavigate();
 
 	return (
 		<div className="container">
 			<h1 className="text-center my-3">Artists</h1>
-			<ul className="list-group">
-				{store.artist.map((item) => {
-					return (
-						<li key={item.id} className="list-group-item">
-							<div className="row">
-								<div className="col-2 d-flex align-items-center justify-content-center">
-									<img src={item.img_url} className="img-thumbnail rounded-circle" />
+			{store.auth == false ? <Navigate to="/" /> :
+				<ul className="list-group">
+					{store.artist.map((item) => {
+						return (
+							<li key={item.id} className="list-group-item">
+								<div className="row">
+									<div className="col-2 d-flex align-items-center justify-content-center">
+										<img src={item.img_url} className="img-thumbnail rounded-circle" />
+									</div>
+									<div className="col-6">
+										<p className="fs-5 fw-bold">{item.name}</p>
+									</div>
+									<div className="col-4 d-flex align-items-center justify-content-evenly">
+										<button onClick={()=> {actions.addFavoriteArtist(item.id)}} className="btn btn-success">Add to Favorites</button>
+										<button onClick={()=> {actions.deleteFavoriteArtist(item.id)}} className="btn btn-danger">Delete from Favorites</button>
+									</div>
 								</div>
-                                <div className="col-6">
-									<p className="fs-5 fw-bold">{item.name}</p>
-								</div>
-								<div className="col-4 d-flex align-items-center justify-content-evenly">
-                                    <button onClick={()=> {actions.addFavoriteArtist(item.id)}} className="btn btn-success">Add to Favorites</button>
-									<button onClick={()=> {actions.deleteFavoriteArtist(item.id)}} className="btn btn-danger">Delete from Favorites</button>
-								</div>
-							</div>
-						</li>
-					);
-				})}
-			</ul>
+							</li>
+						);
+					})}
+				</ul>
+			}
 			<br></br>
 		</div>
 	);
