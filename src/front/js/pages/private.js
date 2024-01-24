598e872e15b276860492a4d572df4976dae76be1
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-24 23:23:03 +0000

    added section albums

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -77,6 +77,22 @@ export const Private = () => {
                         </div>
                         <div className="col-3 border border-primary rounded">
                             <h2>Favorite Albums</h2>
+                            <ul className="list-group">
+                                {store.favoriteArtists.map((item) => {
+                                    return (
+                                        <li key={item.artist_id} className="list-group-item">
+                                            <div className="row">
+                                                <div className="col-10">
+                                                    <p className="fs-5 fw-bold">{item.artist}</p>
+                                                </div>
+                                                <div className="col-2 d-flex align-items-center justify-content-evenly">
+                                                    <button onClick={()=> {actions.deleteFavoriteArtist(item.artist_id)}} className="btn btn-danger">Delete from Favorites</button>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
                         </div>
                         <div className="col-3 border border-primary rounded">
                             <h2>Favorite Songs</h2>
