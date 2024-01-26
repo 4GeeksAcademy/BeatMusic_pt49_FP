2ff958054b9f94faebe42b942bb370890a0a925c
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-26 17:16:26 +0000

    added Navigate

--- src/front/js/component/listSong.js
+++ src/front/js/component/listSong.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 
 export const ListSong = () => {
 	const { store, actions } = useContext(Context);
