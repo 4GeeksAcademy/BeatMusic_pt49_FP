adbfdac80926d89dd1364105e3f822181c9dd05e
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-27 22:44:21 +0000

    finished component

--- src/front/js/pages/signup2.js
+++ src/front/js/pages/signup2.js
@@ -1,38 +1,38 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Artists from "../component/artists";
 import Albums from "../component/albums";
 import Songs from "../component/songs";
 
 export const Signup2 = () => {
     const { store, actions } = useContext(Context)
+    const navigate = useNavigate();
     const [name, setName] = useState('')
 
     function sendData(e) {
         e.preventDefault(name)
-        actions.signup(email, password)
+        actions.editProfileName(name)
+        navigate("/private/" + store.userId)
     }
 
     return (
         <div className="container mt-5">
-            <h4>Choose your name and add your favorites. When you're done, press "Submit".</h4>
-            <div className="row">
-                <div className="col-md-6">
-                    <form>
-                        <div className="mb-3">
-                            <label htmlFor="nameInput" className="form-label">Name:</label>
-                            <input value={name} onChange={(e) => setName(e.target.value)} type="text" className="form-control" id="nameInput" />
-                        </div>
-                    </form>
+            <h4 className="text-center">Choose your name and add your favorites. When you're done, press "Submit".</h4>
+            <br></br>
+            <form className="row row-cols-lg-auto g-3 align-items-center">
+                <div className="col-12">
+                    <label htmlFor="nameInput" className="form-label">Name:</label>
                 </div>
-                <div className="col-md-6">
-                    <Link to={"/private/" + store.userId}>
-                        <button onClick={sendData} type="submit" className="btn btn-primary">Submit</button>
-                    </Link>
+                <div className="col-12">
+                    <input value={name} onChange={(e) => setName(e.target.value)} type="text" className="form-control" id="nameInput" />
                 </div>
-            </div>
+                <div className="col-12">
+                    <button onClick={sendData} type="submit" className="btn btn-primary">Submit</button>
+                </div>
+            </form>
+                
             <div className="row">
                 <div className="col-md-4">
                     <Artists />
