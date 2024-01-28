af585f32e9e6fe326cd36b3f805da0122a34666b
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-28 17:42:47 +0000

    added search bar and cleared code

--- src/front/js/component/albums.js
+++ src/front/js/component/albums.js
@@ -6,6 +6,8 @@ import { Navigate } from "react-router-dom";
 export const Albums = () => {
 	const { store, actions } = useContext(Context);
 	const favorites = store.favoriteAlbums.map(item => item.album)
+	const [searchAlbum, setSearchAlbum] = useState("")
+	const filteredAlbums = store.album.filter(item => item.name.toLowerCase().includes(searchAlbum.toLowerCase()))
 
 	useEffect(() => {
         if (store.favoriteAlbums) {
@@ -17,30 +19,31 @@ export const Albums = () => {
 		<div className="container">
 			<h1 className="text-center my-3">Albums</h1>
 			{store.auth == false ? <Navigate to="/" /> :
-				<ul className="list-group">
-					{store.album.map((item) => {
-						return (
-							<li key={item.id} className="list-group-item">
-								<div className="row">
-									<div className="col-2 d-flex align-items-center justify-content-center">
-										<img src={item.img_url} className="img-thumbnail rounded-circle" />
+				<div>
+					<input className="form-control mb-3" type="text" placeholder="Search by album name" value={searchAlbum} onChange={(e) => setSearchAlbum(e.target.value)} />
+					<ul className="list-group">
+						{filteredAlbums.map((item) => {
+							return (
+								<li key={item.id} className="list-group-item">
+									<div className="row">
+										<div className="col-2 d-flex align-items-center justify-content-center">
+											<img src={item.img_url} className="img-thumbnail rounded-circle" />
+										</div>
+										<div className="col-6">
+											<p className="fs-5 fw-bold">{item.name}</p>
+										</div>
+										<div className="col-4 d-flex align-items-center justify-content-evenly">
+											{favorites.includes(item.name) ?
+												<button onClick={() => { actions.deleteFavoriteAlbum(item.id) }} className="btn btn-danger">Delete from Favorites</button> :
+												<button onClick={() => { actions.addFavoriteAlbum(item.id) }} className="btn btn-success">Add to Favorites</button>
+											}
+										</div>
 									</div>
-									<div className="col-6">
-										<p className="fs-5 fw-bold">{item.name}</p>
-									</div>
-									<div className="col-4 d-flex align-items-center justify-content-evenly">
-										{favorites.includes(item.name) ? null :
-											<button onClick={()=> {actions.addFavoriteAlbum(item.id)}} className="btn btn-success">Add to Favorites</button>
-										}
-										{favorites.includes(item.name) ?
-											<button onClick={()=> {actions.deleteFavoriteAlbum(item.id)}} className="btn btn-danger">Delete from Favorites</button>
-										: null }
-									</div>
-								</div>
-							</li>
-						);
-					})}
-				</ul>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
 			}
 			<br></br>
 		</div>
