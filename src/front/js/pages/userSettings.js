9bde9dc1c767a17f9a092b019a0e391e03fa12df
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-28 00:21:47 +0000

    finished component

--- src/front/js/pages/userSettings.js
+++ src/front/js/pages/userSettings.js
@@ -1,35 +1,70 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
-import { Link, useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 export const UserSettings = () => {
     const { store, actions } = useContext(Context)
     const navigate = useNavigate();
-    const [name, setName] = useState('')
+    const [name, setName] = useState(store.username)
+    const [password, setPassword] = useState('')
+    const [repeatPassword, setRepeatPassword] = useState('')
 
-    function sendData(e) {
-        e.preventDefault(name)
+    function sendName(e) {
+        e.preventDefault()
         actions.editProfileName(name)
         navigate("/private/" + store.userId)
+    }
+    function sendPassword(e) {
+        e.preventDefault()
+        if (password === repeatPassword) {
+            actions.editPassword(password)
+            navigate("/private/" + store.userId)
+        } else {
+            alert("Passwords do not match");
+        }
     }
 
     return (
         <div className="container mt-5">
-            <h4 className="text-center">Choose your name and add your favorites. When you're done, press "Submit".</h4>
-            <br></br>
-            <form className="row row-cols-lg-auto g-3 align-items-center">
-                <div className="col-12">
-                    <label htmlFor="nameInput" className="form-label">Name:</label>
-                </div>
-                <div className="col-12">
-                    <input value={name} onChange={(e) => setName(e.target.value)} type="text" className="form-control" id="nameInput" />
-                </div>
-                <div className="col-12">
-                    <button onClick={sendData} type="submit" className="btn btn-primary">Submit</button>
-                </div>
-            </form>
-                
+            {store.auth == false ? <Navigate to="/" /> :
+                <>
+                    <h1 className="text-center">Account Settings</h1>
+                    <br></br>
+                    <h3>Change your name.</h3>
+                    <form onSubmit={sendName} className="row align-items-center my-3">
+                        <div className="col-2">
+                            <label htmlFor="nameInput" className="form-label">Name:</label>
+                        </div>
+                        <div className="col-auto">
+                            <input value={name} onChange={(e) => setName(e.target.value)} type="text" className="form-control" id="nameInput" required />
+                        </div>
+                        <div className="col-auto">
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                    <h3 className="mt-5">Change your password.</h3>  
+                    <form onSubmit={sendPassword} className="row align-items-center my-3">
+                        <div className="col-2">
+                            <label htmlFor="passwordInput" className="form-label">New Password:</label>
+                        </div>
+                        <div className="col-auto">
+                            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" className="form-control" id="passwordInput" required />
+                        </div>
+                        <div className="col-auto">
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                    <form className="row align-items-center my-2">
+                        <div className="col-2">
+                            <label htmlFor="repeatPasswordInput" className="form-label">Repeat Password:</label>
+                        </div>
+                        <div className="col-auto">
+                            <input value={repeatPassword} onChange={(e) => setRepeatPassword(e.target.value)} type="password" className="form-control" id="repeatPasswordInput" required />
+                        </div>
+                    </form>
+                </>
+            }
         </div>
     );
 };
