6af771f9d9250731ab4794c9e850ccf8a1207fdd
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-31 11:42:08 +0000

    fixed bug

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -21,7 +21,7 @@ export const Navbar = () => {
       			<button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2" aria-controls="offcanvasNavbar2" aria-label="Toggle navigation">
         			<span className="navbar-toggler-icon"></span>
       			</button>
-				<div className="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasNavbar2" aria-labelledby="offcanvasNavbar2Label">
+				<div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasNavbar2" aria-labelledby="offcanvasNavbar2Label">
 					<div className="offcanvas-header">
 						<h5 className="offcanvas-title" id="offcanvasNavbar2Label">Menu</h5>
 						<button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
