740b886ac1b44815e8eb42417303d3c0b9683dfa
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-31 15:57:54 +0000

    finished src/front/js/component/artists.js component

--- src/front/js/component/artists.js
+++ src/front/js/component/artists.js
@@ -17,22 +17,22 @@ export const Artists = () => {
 
     return (
         <div className="container">
-            <h1 className="text-center my-3">Artists</h1>
+            <h1 className="display-2 my-5 link-green">Artists</h1>
             {store.auth == false ? <Navigate to="/" /> :
                 <div>
-                    <input className="form-control mb-3" type="text" placeholder="Search by artist name" value={searchArtist} onChange={(e) => setSearchArtist(e.target.value)} />
+                    <input className="form-control mb-2" type="text" placeholder="Search by artist name" value={searchArtist} onChange={(e) => setSearchArtist(e.target.value)} />
                     <ul className="list-group">
                         {filteredArtists.map((item) => {
                             return (
-                                <li key={item.id} className="list-group-item">
+                                <li key={item.id} className="list-group-item border-0 my-2">
                                     <div className="row">
-                                        <div className="col-2 d-flex align-items-center justify-content-center">
+                                        <div className="col-sm-4 col-lg-2 d-flex align-self-center">
                                             <img src={item.img_url} className="img-thumbnail rounded-circle" />
                                         </div>
-                                        <div className="col-6">
-                                            <p className="fs-5 fw-bold">{item.name}</p>
+                                        <div className="col-sm-6 col-lg-8 d-flex align-self-center">
+                                            <h1 className="display-5 fw-bold">{item.name}</h1>
                                         </div>
-                                        <div className="col-4 d-flex align-items-center justify-content-evenly">
+                                        <div className="col-sm-2 co-lg-2 d-flex align-self-center justify-content-evenly">
                                             {favorites.includes(item.name) ?
                                                 <button onClick={() => { actions.deleteFavoriteArtist(item.id) }} className="btn btn-danger">Delete from Favorites</button> :
                                                 <button onClick={() => { actions.addFavoriteArtist(item.id) }} className="btn btn-success">Add to Favorites</button>
