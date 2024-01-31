c3411ecf417f81411adc94ae75b209650c9b278e
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-31 17:54:16 +0000

    finished src/front/js/component/listAlbum.js component

--- src/front/js/component/listAlbum.js
+++ src/front/js/component/listAlbum.js
@@ -12,22 +12,22 @@ export const ListAlbum = () => {
 				<>
 					<div className="d-flex justify-content-end my-3">
 						<Link to="/admin/newalbum">
-							<button className="btn btn-success">Add new album</button>
+							<button className="btn btn-success btn-green">Add new album</button>
 						</Link>
 					</div>
 					<ul className="list-group">
 						<li className="list-group-item">
 							<div className="row">
-								<div className="col-2">
+								<div className="col-2 d-flex align-self-center">
 									<p className="fs-5 fw-bold">Picture</p>
 								</div>
-								<div className="col-2">
+								<div className="col-2 d-flex align-self-center justify-content-center">
 									<p className="fs-5 fw-bold">Album ID</p>
 								</div>
-								<div className="col-6">
+								<div className="col-6 d-flex align-self-center">
 									<p className="fs-5 fw-bold">Name</p>
 								</div>
-								<div className="col-2">
+								<div className="col-2 d-flex align-self-center">
 									<p className="fs-5 fw-bold">Edit / Delete</p>
 								</div>
 							</div>
@@ -39,13 +39,13 @@ export const ListAlbum = () => {
 										<div className="col-2 d-flex align-items-center justify-content-center">
 											<img src={item.img_url} className="img-thumbnail rounded-circle" />
 										</div>
-										<div className="col-2">
+										<div className="col-2 d-flex align-self-center justify-content-center">
 											<p className="fs-5 fw-bold">{item.id}</p>
 										</div>
-										<div className="col-6">
+										<div className="col-6 d-flex align-self-center">
 											<p className="fs-5 fw-bold">{item.name}</p>
 										</div>
-										<div className="col-2 d-flex align-items-center justify-content-evenly">
+										<div className="col-2 d-flex align-self-center">
 											<Link to={"/admin/editalbum/" + item.id}>
 												<button className="btn text-dark">
 													<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
