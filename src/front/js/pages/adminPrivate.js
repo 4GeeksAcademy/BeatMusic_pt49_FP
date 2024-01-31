6686192a469c2ee6c41711b2472d077161dec74d
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-31 11:42:24 +0000

    finished src/front/js/pages/adminPrivate.js component

--- src/front/js/pages/adminPrivate.js
+++ src/front/js/pages/adminPrivate.js
@@ -10,17 +10,18 @@ export const AdminPrivate = () => {
         <div className="text-center mt-5">
             {store.authAdmin == false ? <Navigate to="/" /> :
                 <>
-                    <h1>PrivateAdmin</h1>
+                    <h1 className="text-white">Private Admin Area</h1>
                     <br></br>
-                    <p>Welcome to your private area.</p>
+                    <p className="text-white">Select what you'd like to manage.</p>
+                    <br></br>
                     <Link to="/admin/listalbum">
-                        <button className="btn btn-primary mx-1">Admin List Album</button>
+                        <button className="btn btn-primary btn-green my-2 mx-2">Admin List Album</button>
                     </Link>
                     <Link to="/admin/listartist">
-                        <button className="btn btn-primary mx-1">Admin List Artist</button>
+                        <button className="btn btn-primary btn-green my-2 mx-2">Admin List Artist</button>
                     </Link>
                     <Link to="/admin/listsong">
-                        <button className="btn btn-primary mx-1">Admin List Song</button>
+                        <button className="btn btn-primary btn-green my-2 mx-2">Admin List Song</button>
                     </Link>
                 </>
             }
