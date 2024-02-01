46d16c15d476f1d3cf6bed9ecea2ff1e5017307f
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-02-01 20:13:20 +0000

    finished src/front/js/component/editArtist.js component

--- src/front/js/component/editArtist.js
+++ src/front/js/component/editArtist.js
@@ -28,21 +28,21 @@ export const EditArtist = () => {
     }
 
     return (
-        <div className="container mt-5">
+        <div className="container mt-3">
             {store.authAdmin == false ? <Navigate to="/" /> :
                 <>
-                    <h1 className="text-center mt-3">Edit an Artist</h1>
+                    <h1 className="text-center my-4 link-green">Edit an Artist</h1>
                     <div className="col-md-6">
                         <form onSubmit={sendData}>
                             <div className="mb-3">
-                                <label htmlFor="nameInput" className="form-label">Artist Name</label>
+                                <label htmlFor="nameInput" className="form-label text-white">Artist Name</label>
                                 <input value={name} onChange={(e) => setName(e.target.value)} className="form-control" id="nameInput" aria-describedby="emailHelp" required />
                             </div>
                             <div className="mb-3">
-                                <label htmlFor="urlInput" className="form-label">Image URL</label>
+                                <label htmlFor="urlInput" className="form-label text-white">Image URL</label>
                                 <input value={url} onChange={(e) => setUrl(e.target.value)} className="form-control" id="urlInput" required />
                             </div>
-                            <button type="submit" className="btn btn-primary">Submit</button>
+                            <button type="submit" className="btn text-white btn-green">Submit</button>
                         </form>
                     </div>
                 </>
